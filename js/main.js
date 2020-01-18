b9e7a426de550ee38e1877f14f7f57fee9bc8988
Lisa Wilkins <lisa@lisawilkins.com>
2020-01-18 14:28:53 -0800

    remove elevator

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$( document ).ready(function() {
-
-$('.btn-toggle').click(function(){
-    // $('.scroller').toggle("slow");
-    $( '.scroller' ).toggle('slow');
-  });
-});
+// $( document ).ready(function() {
+//
+// $('.btn-toggle').click(function(){
+//     // $('.scroller').toggle("slow");
+//     $( '.scroller' ).toggle('slow');
+//   });
+// });
